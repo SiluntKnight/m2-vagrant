0fb0e056a2e521975672c52db44a631e39037e8f
Simon Lunt <slunt32@gmail.com>
2018-06-08 23:04:25 +0100

    added php7 fpm to install script

--- provision/setup.sh
+++ provision/setup.sh
@@ -20,7 +20,7 @@ add-apt-repository ppa:ondrej/php -y > /dev/null
 apt-get update > /dev/null
 
 echo "Installing PHP"
-apt-get install -y php7.0 libapache2-mod-php7.0 php7.0 php7.0-common php7.0-gd php7.0-mysql php7.0-mcrypt php7.0-curl php7.0-intl php7.0-xsl php7.0-mbstring php7.0-zip php7.0-bcmath php7.0-iconv php7.0-soap > /dev/null
+apt-get install -y php7.0 libapache2-mod-php7.0 php7.0 php7.0-common php7.0-gd php7.0-mysql php7.0-mcrypt php7.0-curl php7.0-intl php7.0-xsl php7.0-mbstring php7.0-zip php7.0-bcmath php7.0-iconv php7.0-soap php7.0-fpm > /dev/null
 
 echo "Prepping MySQL"
 apt-get install debconf-utils -y > /dev/null
