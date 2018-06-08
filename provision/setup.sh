df856aa29a664528adef35b7a366f48cb30aa76b
Simon Lunt <slunt32@gmail.com>
2018-06-08 22:53:17 +0100

    add vagrantfile

---
+++ provision/setup.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+echo "Provisioning virtual machine..."
+
+echo "Installing Git"
+apt-get install git -y > /dev/null
+    
+echo "Installing Nginx"
+apt-get install nginx -y > /dev/null
+
+echo "Configuring Nginx"
+cp /var/www/provision/config/nginx_vhost /etc/nginx/sites-available/nginx_vhost > /dev/null
+ln -s /etc/nginx/sites-available/nginx_vhost /etc/nginx/sites-enabled/
+rm -rf /etc/nginx/sites-available/default
+service nginx restart > /dev/null
+
+echo "Updating PHP repository"
+apt-get install python-software-properties build-essential -y > /dev/null
+add-apt-repository ppa:ondrej/php5 -y > /dev/null
+apt-get update > /dev/null
+
+echo "Installing PHP"
+add-apt-repository ppa:ondrej/php
+apt-get -y update
+apt-get install -y php7.0 libapache2-mod-php7.0 php7.0 php7.0-common php7.0-gd php7.0-mysql php7.0-mcrypt php7.0-curl php7.0-intl php7.0-xsl php7.0-mbstring php7.0-zip php7.0-bcmath php7.0-iconv php7.0-soap
+
+echo "Prepping MySQL"
+apt-get install debconf-utils -y > /dev/null
+debconf-set-selections <<< "mysql-server mysql-server/root_password password 1234"
+debconf-set-selections <<< "mysql-server mysql-server/root_password_again password 1234"
+
+echo "Installing MySQL"
+apt-get install mysql-server -y > /dev/null
+
